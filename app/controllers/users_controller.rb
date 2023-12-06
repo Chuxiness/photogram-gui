1431f5d51cbdd579a6ba22c2d8d66a085ebc3e44
Chuxiness <146590179+Chuxiness@users.noreply.github.com>
2023-12-06 08:08:46 +0000

    some of the user details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def show
+    url_username = params.fetch("path_username")
+    matching_usernames = User.where({:username => url_username})
+    @the_user = matching_usernames.at(0)
+    render({ :template => "user_templates/show"})
+  end
+  
+  
+  
   def index
     matching_users = User.all
     @list_of_users= matching_users.order({:username => :asc})
