1cb820c22a34fe20f0e2674896dd3268bd9c7d72
Chuxiness <146590179+Chuxiness@users.noreply.github.com>
2023-12-07 00:42:09 +0000

    moree stuff

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,9 +1,17 @@
 class PhotosController < ApplicationController
-def index
+
+def show
+  url_id=params.fetch("path_id")
+  @the_photo = Photo.where({:id => url_id}).at(0)
+  render({ :template => "photo_templates/show"})
+end
+
+
+
+  def index
   matching_photos = Photo.all
   @list_of_photos = matching_photos.order({ :created_at => :desc })
   render({ :template => "photo_templates/index"})
 end
-
 
 end
